2c2e96ecd6dbe05fe204d272c6fb0ba6b51c0d91
Yaroslav Krinitsky <yaroskrinitsky@gmail.com>
2020-05-11 04:57:37 +0300

    mortgagecheck vm added

---
+++ ViewModels/MortgageCheckViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MortgageWebAPI.ViewModels
+{
+    public class MortgageCheckViewModel
+    {
+        private double _monthlyCost;
+        public bool IsFeasible { get; set; }
+
+        public double MonthlyCost
+        {
+            get => Math.Round(_monthlyCost, 2, MidpointRounding.AwayFromZero);
+            set => _monthlyCost = value;
+        }
+    }
+}
