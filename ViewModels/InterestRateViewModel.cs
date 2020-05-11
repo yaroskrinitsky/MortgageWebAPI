55bc5a6a284dd8f255cbc483e81b269d31da358e
Yaroslav Krinitsky <yaroskrinitsky@gmail.com>
2020-05-11 04:57:48 +0300

    interestrate vm added

---
+++ ViewModels/InterestRateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace MortgageWebAPI.ViewModels
+{
+    public class InterestRateViewModel
+    {
+        [JsonPropertyName("maturityPeriod")]
+        public int MaturityPeriod { get; set; }
+        [JsonPropertyName("interestRate")]
+        public double InterestRate { get; set; }
+        [JsonPropertyName("lastUpdate")]
+        public DateTime LastUpdate { get; set; }
+    }
+}
