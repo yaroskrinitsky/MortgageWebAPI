acefb3e8a3db46727af238d947a21327f1a92c47
Yaroslav Krinitsky <yaroskrinitsky@gmail.com>
2020-05-11 05:01:23 +0300

    error handling enabled

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MortgageWebAPI.Controllers
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/api/error")]
+        public IActionResult Error() => Problem();
+    }
+}
