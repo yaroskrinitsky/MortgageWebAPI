53069f6b1437daab099219108aa2f93f7102aed0
Yaroslav Krinitsky <yaroskrinitsky@gmail.com>
2020-05-11 05:01:12 +0300

    mortgage api model for post data added with validation

---
+++ ApiModels/MortgageCheckApiModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MortgageWebAPI.ApiModels
+{
+    public class MortgageCheckApiModel
+    {
+        [Required]
+        [JsonPropertyName("income")]
+        public double Income { get; set; }
+        [Required]
+        [JsonPropertyName("maturityPeriod")]
+        public int MaturityPeriod { get; set; }
+        [Required]
+        [JsonPropertyName("loanValue")]
+        public double LoanValue { get; set; }
+        [Required]
+        [JsonPropertyName("homeValue")]
+        public double HomeValue { get; set; }
+    }
+}
