a565f18bfecfa71d6095f72f54d772bedefc2430
Yaroslav Krinitsky <yaroskrinitsky@gmail.com>
2020-05-13 14:58:19 +0300

    redundant commentaries removed

--- MortgageWebAPI.Tests/MortgageServiceTests.cs
+++ MortgageWebAPI.Tests/MortgageServiceTests.cs
@@ -1,7 +1,15 @@
-﻿namespace MortgageWebAPITest
+﻿using MortgageWebAPI.Services;
+using Xunit;
+
+namespace MortgageWebAPITest
 {
     public class MortgageServiceTests
     {
-        
+        [Fact]
+        public void GetMortgageRates_ReturnsRatesList()
+        {
+            var service = new MortgageService();
+            
+        }
     }
 }
